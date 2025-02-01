d085b9e73de4d4e2c842c1ff7220b25a4f07b729
sameersambhare <sameersambhare@gmail.com>
2025-02-01 23:14:58 +0530

    fix the minor issue

--- app/(root)/thread/[id]/page.tsx
+++ app/(root)/thread/[id]/page.tsx
@@ -4,7 +4,7 @@ import { fetchUser } from "@/lib/actions/user.actions";
 import { currentUser } from "@clerk/nextjs/server"
 import { redirect } from "next/navigation";
 import Comment from "@/components/forms/Comment";
-const page = async ({params }: {params: { id: string } }) => {
+const page = async ({ params }: { params: { id: string } }) => {
     if (!params.id) return null
     const user = await currentUser();
     if (!user) return null
@@ -17,7 +17,14 @@ const page = async ({params }: {params: { id: string } }) => {
                 <ThreadCard key={thread._id} id={thread._id} parentId={thread.parentId} content={thread.text} author={thread.author} community={thread.community} createdAt={thread.createdAt} comments={thread.children} currentUserId={user?.id || ""} />
             </div>
             <div className="mt-7 ">
-                <Comment threadId={thread.id} currentUserImage={user.imageUrl} currentUserId={JSON.stringify(userInfo._id)} />
+                <Comment threadId={thread.id} currentUserImage={userInfo.image} currentUserId={JSON.stringify(userInfo._id)} />
+            </div>
+            <div className="mt-10 ">
+                {thread.children.map((childrenItm: any) => {
+                    return (
+                        <ThreadCard key={childrenItm._id} id={childrenItm._id} parentId={childrenItm.parentId} content={childrenItm.text} author={childrenItm.author} community={childrenItm.community} createdAt={childrenItm.createdAt} comments={childrenItm.children} isComment />
+                    )
+                })}
             </div>
         </section>
     )
--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -35,7 +35,7 @@ const ThreadCard = ({
     isComment,
 }: ThreadCardsProps) => {
     return (
-        <article className="flex w-full flex-col rounded-xl bg-dark-2 p-7">
+        <article className={`flex w-full flex-col rounded-xl ${isComment ? 'px-0 xs:px-7' : 'bg-dark-2 p-7'}`}>
             <div className="flex justify-between items-start">
                 <div className="flex w-full flex-1 flex-row gap-4">
                     <div className="flex flex-col items-center">
