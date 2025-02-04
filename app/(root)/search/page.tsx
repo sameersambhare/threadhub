02b01edf9cdc46d2999ec02681ba74db385a0d7d
sameersambhare <sameersambhare@gmail.com>
2025-02-05 00:38:56 +0530

    add the usercard

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -1,21 +1,34 @@
-import { fetchUser } from "@/lib/actions/user.actions"
+import UserCard from "@/components/cards/UserCard"
+import { fetchAllUsers, fetchUser } from "@/lib/actions/user.actions"
 import { currentUser } from "@clerk/nextjs/server"
 import { redirect } from "next/navigation"
 
-const page = async() => {
-    const user=await currentUser()
-    if(!user) return null
-    const userInfo=await fetchUser(user.id)
-    if(!userInfo?.onboarded) redirect('/onboarding')
+const page = async () => {
+    const user = await currentUser()
+    if (!user) return null
+    const userInfo = await fetchUser(user.id)
+    if (!userInfo?.onboarded) redirect('/onboarding')
 
-    //to fetch all the users
-    
+    const result = await fetchAllUsers({ userId: user.id, searchString: '', pageNumber: 1, pageAmount: 25, sortBy: "desc" });
+
     return (
-       
+
         <section>
             <h1 className="head-text mb-10">
                 Search
             </h1>
+            <div className="mt-14 flex flex-col gap-9">
+                {result?.users.length===0?(
+                    <p className="no-results">No Users</p>
+                ):(
+                    <>
+                    {result?.users.map((person)=>(
+                        <UserCard/>
+                    ))}
+                    </>
+                )}
+
+            </div>
         </section>
     )
 }
